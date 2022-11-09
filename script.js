ecbb9df385685064b27d431298f8b60d1ee6f45a
kirti <kirtimakhija7489@gmail.com>
2022-11-09 20:34:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+gsap.from("#page1-center h1",{
+    opacity: 0,
+    duration: 2
+})
+gsap.from("#image1",{
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    delay: 0.2
+})
+gsap.from("#image2",{
+    opacity: 0,
+    x: 100,
+    duration: 1,
+    delay: 0.2
+})
+gsap.from("#image3",{
+    opacity: 0,
+    y: -100,
+    duration: 1,
+    delay: 0.2
+})
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+var a1 = document.querySelector("#a1")
+var divOverlay = document.querySelector("#div-overlay")
+var flag = 0
+a1.addEventListener("click",function(){
+    if(flag === 0){
+        divOverlay.style.opacity = 1
+        flag = 1;
+    }
+    else{
+        divOverlay.style.opacity = 0
+        flag = 0;
+    }
+})
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
